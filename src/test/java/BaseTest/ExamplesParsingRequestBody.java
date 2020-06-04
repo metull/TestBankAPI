517eb3a5fdacee0c320a1d6e1b21af2ce68583a2
Adriel Brekke <metull21@gmail.com>
2020-06-04 14:51:40 +0300

    remove not need methods

--- src/test/java/BaseTest/ExamplesParsingRequestBody.java
+++ src/test/java/BaseTest/ExamplesParsingRequestBody.java
@@ -27,33 +27,4 @@ public class ExamplesParsingRequestBody {
         }
         return true;
     }
-
-    public boolean recursiveObjectCheck2(String bind, Object property) {
-        if (property == null) {
-            System.out.println(String.format("Element '%s' was null", bind));
-        }
-        if (property instanceof Map) {
-      boolean bool = ((Map<?,?>) property).entrySet().stream().allMatch
-                    (o -> recursiveObjectCheck2(bind + "-> " +  o.getKey(), o.getValue()));
-            System.out.println("Map");
-            return bool;
-        }
-        if (property instanceof Collection) {
-            int i = 0;
-            boolean bool = ((List<?>) property).stream().allMatch
-                    (o -> recursiveObjectCheck2(bind + "[" + i + "]", ((List<?>) property).get(i)));
-            System.out.println("Collection");
-            return  bool;
-        }
-        return true;
-    }
-
-    public void checkForEquality(String json, String json2) throws IOException {
-        final ObjectMapper mapper = new ObjectMapper();
-
-        final JsonNode tree1 = mapper.readTree(json);
-        final JsonNode tree2 = mapper.readTree(json2);
-
-        assert tree1.equals(tree2);
-    }
 }
